ed453ee9d078b068f1fd21de6aa2247cbc146203
dalazhi <45230013+dalazhi@users.noreply.github.com>
2022-08-15 19:24:31 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,21 @@
+export default {
+async fetch(request, env) {
+const cars = [
+//"huku01.herokuapp.com",
+//"huku02.herokuapp.com",
+//"huku03.herokuapp.com",
+//"huku05.herokuapp.com",
+  "goindex.dalazhi.workers.dev"
+];
+//let host = cars[Math.floor(Math.random() * cars.length)]; //随机选择VPS
+let host = cars[new Date().getDate() % cars.length]; //每天自动更换VPS
+
+let url = new URL(request.url);
+if (url.pathname.startsWith('/')) {
+url.hostname = host;
+let new_request = new Request(url,request);
+return fetch(new_request);
+}
+return env.ASSETS.fetch(request);
+}
+};
